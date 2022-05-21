a9166a6d6acf482788da00ed3cb6c47b1a3d0e79
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2022-05-21 12:58:08 +0300

    some fix

--- js/sidebars.js
+++ js/sidebars.js
@@ -2,11 +2,28 @@ const leftsideBtn = document.querySelector(".left-side-btn");
 const rightsideBtn = document.querySelector(".right-side-btn");
 const leftSidebar = document.querySelector(".window__left-sidebar");
 const rightSidebar = document.querySelector(".window__right-sidebar");
+const wndwBody = document.querySelector(".window__body");
 
 leftsideBtn.addEventListener("click", () => {
     leftSidebar.classList.toggle("is-hidden");
+    if (
+        !leftSidebar.classList.contains("is-hidden") ||
+        !rightSidebar.classList.contains("is-hidden")
+    ) {
+        wndwBody.classList.add("with-sidebars");
+    } else {
+        wndwBody.classList.remove("with-sidebars");
+    }
 });
 
 rightsideBtn.addEventListener("click", () => {
     rightSidebar.classList.toggle("is-hidden");
+    if (
+        !leftSidebar.classList.contains("is-hidden") ||
+        !rightSidebar.classList.contains("is-hidden")
+    ) {
+        wndwBody.classList.add("with-sidebars");
+    } else {
+        wndwBody.classList.remove("with-sidebars");
+    }
 });
