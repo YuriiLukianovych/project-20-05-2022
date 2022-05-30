ba6f818f938c62170b90392d45074316b12e003f
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2022-05-30 13:43:19 +0300

    erfe

--- src/js/modules/profile.js
+++ src/js/modules/profile.js
@@ -1,13 +1,20 @@
 export const profile = () => {
     const logBtn = document.querySelector(".sign-in-up");
     const logMenu = document.querySelector(".login-menu");
-    const profileBtnList = document.querySelectorAll("[data-profile-btn]");
+    const profileBtnList = document.querySelectorAll("[data-profile_btn]");
     const profilePopupBtnList = document.querySelectorAll(
-        "li[data-profile-popup]"
+        "li[data-profile_popup]"
     );
     const profilePopup = document.querySelector(".profile-popup");
     const popupCloseBtn = document.querySelector(
         "[data-profile-popup-close-btn]"
+    );
+    const popupContentList = document.querySelectorAll(
+        "div[data-profile_popup]"
+    );
+    const popupProfileOK = document.querySelector("[data-profile-popup-ok]");
+    const popupProfileCANCEL = document.querySelector(
+        "[data-profile-popup-cancel]"
     );
 
     logBtn.addEventListener("click", () => {
@@ -31,11 +38,55 @@ export const profile = () => {
     profileBtnList.forEach((btn) => {
         btn.addEventListener("click", () => {
             profilePopup.classList.remove("is-hidden");
+            let currentItem = btn.dataset.profile_btn;
+            console.log(currentItem);
+
+            profilePopupBtnList.forEach((item) => {
+                item.classList.remove("active-popup-profile-item");
+            });
+            popupContentList.forEach((div) => {
+                div.classList.add("dn");
+            });
+
+            [...profilePopupBtnList]
+                .find((btn) => btn.dataset.profile_popup === currentItem)
+                .classList.add("active-popup-profile-item");
+
+            [...popupContentList]
+                .find((div) => div.dataset.profile_popup === currentItem)
+                .classList.remove("dn");
+        });
+    });
+
+    profilePopupBtnList.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            let currentItem = btn.dataset.profile_popup;
+
+            profilePopupBtnList.forEach((item) => {
+                item.classList.remove("active-popup-profile-item");
+            });
+
+            popupContentList.forEach((div) => {
+                div.classList.add("dn");
+            });
+
+            btn.classList.add("active-popup-profile-item");
+
+            [...popupContentList]
+                .find((div) => div.dataset.profile_popup === currentItem)
+                .classList.remove("dn");
         });
     });
 
     popupCloseBtn.addEventListener("click", () => {
-        console.log(profilePopupBtnList);
+        profilePopup.classList.add("is-hidden");
+    });
+
+    popupProfileOK.addEventListener("click", () => {
+        profilePopup.classList.add("is-hidden");
+    });
+
+    popupProfileCANCEL.addEventListener("click", () => {
         profilePopup.classList.add("is-hidden");
     });
 };
